7429854d4614394d2a77819541c204f9ea263c56
David A <david.allert@hotmail.com>
2025-05-08 16:23:42 +0200

    Updated tests according to feedback.

--- backend/test/test_get_email.py
+++ backend/test/test_get_email.py
@@ -34,23 +34,16 @@ def test_get_user_by_no_email(user_data):
   mocked_dao.find.return_value = user_data
 
   user_controller = UserController(dao=mocked_dao)
-  with pytest.raises(ValueError) as result:
-    user_controller.get_user_by_email("")
-
-
-
-  assert str(result.value) == 'Error: invalid email address'
+  with pytest.raises(ValueError, match="Error: invalid email address"):
+        user_controller.get_user_by_email("")
 
 def test_get_user_by_wrong_format_email(user_data):
   mocked_dao = mock.MagicMock()
   mocked_dao.find.return_value = user_data
 
   user_controller = UserController(dao=mocked_dao)
-  with pytest.raises(ValueError) as result:
-    user_controller.get_user_by_email("jane.doegmail")
-
-
-  assert str(result.value) == 'Error: invalid email address'
+  with pytest.raises(ValueError, match="Error: invalid email address"):
+        user_controller.get_user_by_email("jane.doegmail")
 
 def test_get_user_by_email(user_data):
   mocked_dao = mock.MagicMock()
@@ -70,25 +63,23 @@ def test_get_users_by_email(users_data, capsys):
   user_controller = UserController(dao=mocked_dao)
   result = user_controller.get_user_by_email(users_data[0]["email"])
 
+  assert result == {'email': 'jane.doe@gmail.com', 'firstName': 'Jane', 'lastName': 'Doe'}
+
+def test_get_users_by_email_message(users_data, capsys):
+  mocked_dao = mock.MagicMock()
+  mocked_dao.find.return_value = users_data
+
+  user_controller = UserController(dao=mocked_dao)
+  result = user_controller.get_user_by_email(users_data[0]["email"])
+
   captured = capsys.readouterr()
 
   assert f"Error: more than one user found with mail {users_data[0]['email']}" in captured.out
-
-  assert result == {'email': 'jane.doe@gmail.com', 'firstName': 'Jane', 'lastName': 'Doe'}
-
 
 def test_get_user_is_none_by_email():
   mocked_dao = mock.MagicMock()
   mocked_dao.find.return_value = []
 
   user_controller = UserController(dao=mocked_dao)
-  with pytest.raises(IndexError) as result:
+  with pytest.raises(IndexError, match=r".*None.*") as result:
     user_controller.get_user_by_email("hej@hej.com")
-
-  assert str(result.value) == "None"
-
-
-
-
-
-
